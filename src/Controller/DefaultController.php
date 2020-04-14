caf0c572acbfa7cfd2ccb8eff23067e3e0711656
Naren <developer.naren@gmail.com>
2020-04-14 17:05:25 +0400

    added html and md file reader

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -1,40 +1,40 @@
 <?php
 
-/*
- * This file is part of the DriftPHP package.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * Feel free to edit as you please, and have fun.
- *
- * @author Marc Morera <yuhu@mmoreram.com>
- */
-
 declare(strict_types=1);
 
 namespace App\Controller;
 
-use Drift\Kernel;
+use App\Domain\FileResolver;
 use React\Filesystem\Filesystem;
 use React\Promise\FulfilledPromise;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
 
-/**
- * Class DefaultController.
- *
- * You can run this action by making `curl` to /
- */
 class DefaultController
 {
     public function __invoke(Request $request, Filesystem $filesystem, KernelInterface $kernel)
     {
-        return $filesystem->getContents($kernel->getProjectDir() . '/Drift/views/index.html')
-            ->then(function ($content){
-                return new Response($content);
-            });
+
+        $routeParams = $request->attributes->get('_route_params');
+        $fileResolver = new FileResolver($filesystem, $kernel->getProjectDir() . '/Drift/views/' . $routeParams['page']);
+
+        try {
+            return $fileResolver
+                ->toHtml()
+                ->then(function ($content) {
+                    $statusCode = 200;
+                    if (empty($content)) {
+                        $statusCode = 404;
+                        $content = 'Sorry, I feel defeated. I tried to find what you are looking for. Believe me I tried.';
+                    }
+
+                    return new Response($content, $statusCode);
+                });
+        } catch (\Exception $exception) {
+
+            return 'this is a test';
+        }
+
     }
 }
---
+++ src/Domain/FileResolver.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Domain;
+
+use React\Filesystem\Filesystem;
+use React\Promise\Deferred;
+use React\Promise\FulfilledPromise;
+use React\Promise\PromiseInterface;
+use League\CommonMark\GithubFlavoredMarkdownConverter;
+use React\Promise\RejectedPromise;
+
+class FileResolver
+{
+
+    private $filename;
+    private $filesystem;
+
+    public function __construct(Filesystem $filesystem, string $filename)
+    {
+        $this->filename = $filename;
+        $this->filesystem = $filesystem;
+    }
+
+
+    public function toHtml(): PromiseInterface
+    {
+        $deferred = new Deferred();
+
+        $filename = $this->filename . '.html';
+
+        $this->filesystem->file($filename)
+            ->exists()
+            ->then(function () use ($filename, &$deferred) {
+                return $this->filesystem->getContents($filename)
+                    ->then(function ($c) use (&$deferred) {
+                        if (!empty($c)) {
+                            $deferred->resolve($c);
+                        }
+                    });
+            }, function () use (&$deferred) {
+                $filename = $this->filename . '.md';
+                return $this->filesystem->getContents($filename)
+                    ->then(function ($c) use (&$deferred) {
+                        if (!empty($c)) {
+                            $converter = new GithubFlavoredMarkdownConverter([
+                                'html_input' => 'strip',
+                                'allow_unsafe_links' => false,
+                            ]);
+                            $deferred->resolve($converter->convertToHtml($c));
+                        }
+                    }, function () use (&$deferred) {
+                        $deferred->resolve('');
+                    });
+            });
+
+        return $deferred->promise();
+    }
+}
